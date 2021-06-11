4bfeb69ea864b20de095f92142112e9b41b2f465
Anumjawaid <anumjawaid99@gmail.com>
2021-06-11 21:43:25 +0500

    input added

---
+++ src/components/Journal/generalJournal.jsx
@@ -0,0 +1,75 @@
+import react, { useState } from 'react'
+import './style.css'
+
+export default function GeneralJournal() {
+    const [acc,setacc]=useState()
+    const[debit,setdeb]=useState()
+    const[credit,setcred]=useState()
+    const[val,setval]=useState()
+    const[journal,setjournal]=useState()
+   const generjournal={}
+    const addEntry = ()=>{
+        generjournal.acc=acc
+        generjournal.date=datetoday()
+        generjournal.credit=credit
+        generjournal.debit=debit
+        generjournal.value=val
+        setjournal(generjournal)
+        setacc("")
+        setdeb("")
+        setcred("")
+        setval("")
+        console.log(journal,"journal")
+        console.log(generjournal,'generaljournal')
+    }
+
+
+
+    const datetoday =()=>{
+        var month=['Jan',"Feb","Mar","Apr","May","June","July","Aug","Sep","Oct","Nov","Dec"]
+        var days=['Monday',"Tuesday","Wednesday",'Thursday','Friday','Saturday','Sunday']
+        var d = new Date();
+       
+        var date=(d.getUTCDate()+ ","+month[d.getMonth()]+","+d.getFullYear())
+        var day=(days[d.getDay()])
+        return date
+    }
+    // console.log(datetoday)
+    return (
+        <>
+        <div className="conatiner">
+            <div className="row">
+                <div className="col-md-12">
+                    <h3>Write your general Journal Entry</h3>
+                </div>
+            </div>
+            <div className="row">
+                <div className="col-md-12">
+                    <table id='genejou'>
+                        <tr>
+                            <th>Date</th>
+                            <th>Account(Explanation)</th>
+                            <th>Debit</th>
+                            <th>Credit</th>
+                            <th>RadioBtn</th>
+                            <th>Value</th>
+                        </tr>
+                        <tr>
+                            <td>{datetoday()}</td>
+                            <td><input type='text' value={acc} onChange={(e)=>setacc(e.target.value)} placeholder='Account'></input></td>
+                                <td><input type='text'value={debit} onChange={(e)=>setdeb(e.target.value)} placeholder='Debit'></input></td>
+                               <td> <input type='text' value={credit} onChange={(e)=>setcred(e.target.value)} placeholder='Credit'></input></td>
+                               <td> <input type='radio' id='debit' value='debit'name='track'></input><label for='debit'>debit</label><input value='credit'type='radio' id='credit' name='track'></input><label for='credit'>credit</label></td>
+                                <td><input type='text' value={val} onChange={(e)=>setval(e.target.value)} placeholder='Value'></input></td>
+                            
+                        </tr>
+                        <tr>
+                            <td colSpan='6'><button onClick={addEntry}>Add</button></td>
+                            </tr>
+                    </table>
+                </div>
+            </div>
+        </div>
+        </>
+    )
+}
