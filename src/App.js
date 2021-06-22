d93327254131052a8b432a8397e32d41169bd07e
Anumjawaid <anumjawaid99@gmail.com>
2021-06-22 12:56:08 +0500

    taccounts testing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
 import GeneralJournal from './components/Journal/generalJournal';
+import Taccounts from './components/TrialBalance/taccounts'
 
 function App() {
   return (
     <div className="App">
       <GeneralJournal />
+      {/* <Taccounts /> */}
+
+
     </div>
   );
 }
