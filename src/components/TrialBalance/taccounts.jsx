065830e00af5b26ce1c5fe8ce80cf234ee133a0e
Anumjawaid <anumjawaid99@gmail.com>
2021-07-09 21:07:10 +0500

    trial balance useeffect used,working fine

--- src/components/TrialBalance/taccounts.jsx
+++ src/components/TrialBalance/taccounts.jsx
@@ -1,26 +1,40 @@
-import React,{useState,useEffect} from 'react'
+import React,{useState,useEffect, useLayoutEffect} from 'react'
 import firebase from '../configuration/firebase'
 
 
 export default function Taccounts ()  {
 
     const acc=[]
-    var [tac,setTac]=useState()
-    useEffect(()=>{
-        firebase.database().ref('/').child('taccounts').on('child_added',(s)=>{
-            acc.push(s.val())
-            tac=acc
+    var [tac,settac]=useState([])
+    // // firebase.database().ref('/').child('taccounts').on('child_added',(s)=>{
+    // //     acc.push(s.val())
+    // //     // tac=acc
 
-            console.log(s.val(),acc)
-        })
+    //     console.log(s.val(),acc)
+    // })
+    firebase.database().ref('/').child('taccounts').on('child_added',(s)=>(
+        acc.push(s.val())
+    ))
+    // settac(acc)
+    useEffect(()=>{
+       settac(acc)
+
+        
 
     })
-    console.log(acc,"incr")
-    console.log(tac,"incr")
+    // useLayoutEffect(()=>{
+    //     acc.length ? console.log("Nothing",acc.length) :console.log("Something")
+    // })
+    // console.log(tac,"incr")
     return(
         <>
         <h3>T accounts</h3>
-        {<h2>{acc}</h2>}
+        {tac.map((v,i)=>(
+            <h1>{v['entry']} {v['value']}</h1>
+        ))}
+       
+
+        
         </>
     )
 } 
