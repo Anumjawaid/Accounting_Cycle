fa15f4b12adaac01ce35351ccb0603ad5dbea6c3
Anumjawaid <anumjawaid99@gmail.com>
2021-08-09 00:35:20 +0500

    trial balance link route added

--- src/components/configuration/router.js
+++ src/components/configuration/router.js
@@ -3,13 +3,14 @@ import {BrowserRouter as Router ,Route} from 'react-router-dom'
 
 import Taccounts from '../TrialBalance/taccounts'
 import GeneralJournal from '../Journal/generalJournal'
+import TrialBalance from '../TrialBalance/trialbalance'
 class AppRouter extends React.Component{
     render(){
         return(
             <Router>
                 <Route  exact path='/' component={GeneralJournal}></Route> 
                 <Route path='/tacc' component={Taccounts}/>
-                {/* <Route path='/todos' component={FnTodos}></Route> */}
+                <Route path='/trial' component={TrialBalance}></Route>
             </Router>
         )
     }
