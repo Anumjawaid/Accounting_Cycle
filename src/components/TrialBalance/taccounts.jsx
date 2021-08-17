fdb02c46193c01d8247d81402af3d85f4724bcd5
Anumjawaid <anumjawaid99@gmail.com>
2021-08-18 04:38:55 +0500

    taccounts results need to shift in context

--- src/components/TrialBalance/taccounts.jsx
+++ src/components/TrialBalance/taccounts.jsx
@@ -8,8 +8,9 @@ export default function Taccounts ()  {
 
     const acc=[]
     var [tac,settac]=useState([])
-    var [tacb,setttac]=useState([])
-    var a
+    // var [tacb,setttac]=useState([])
+    // var a
+    var trialarr=[]
   
     firebase.database().ref('/').child('Taccounts').on('child_added',(s)=>(
         acc.push(s.val())
@@ -21,7 +22,8 @@ export default function Taccounts ()  {
 
     },[])
     console.log(tac,"val")
-    const ADDval=()=>{
+    var val={}
+    const result=()=>{
 
         console.log("Me chal raha hon")
         console.log(tac,"values")
@@ -33,14 +35,39 @@ export default function Taccounts ()  {
             result.push(v['debit'].reduce(myFunc)-v['credit'].reduce(myFunc))
         ))
         console.log(result)
+        for(var i=0;i<tac.length;i++){
+            if((result[i]-result[i])==0){
+                val.name=tac[i]['name']
+                val.debit=Math.abs(result[i])
+            //     firebase.database().ref('/').child('Result').push(
+            //     val
+            // )
+            trialarr.push(val)
+            }
+            else{
+                val.name=tac[i]['name']
+                val.credit=Math.abs(result[i])
+                // firebase.database().ref('/').child('Result').push(
+                //     val
+                // )
+            trialarr.push(val)
+
+                
+            }
+            console.log(i)
+        }
+       
         return result
 
     }
-    const result=ADDval()
+    // const result=ADDval()
+    // console.log(result)
+
+   
     return(
         <>
         <h3>T accounts</h3>
-        <button onClick={ADDval}>Add</button>
+        {/* <button onClick={ADDval}>Add</button> */}
         {
             tac.map((v,i)=>(
                 <div>
